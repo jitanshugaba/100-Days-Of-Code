82fdd1a4f16b1f88860be4c2fa6345fd9915faac
jitanshugaba <jitanshugaba@gmail.com>
2026-08-11 19:14:33 +0530

    Completed Day 1 & 2

--- DAY1QUESTION1.c
+++ DAY1QUESTION1.c
@@ -15,6 +15,12 @@ Sum = 19
 */
 #include<stdio.h>
 int main(){
+    int a,b,sum;    
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    sum = a + b;
+    printf("Sum = %d",sum);
+ 
 
     return 0 ;
 }
